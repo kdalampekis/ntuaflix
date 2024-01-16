e6b2b373c55abf91eba2236d6b6fa051df2fdcfd
ntua-el21406 <el21406@mail.ntua.gr>
2024-01-16 13:16:28 +0200

    Changes to be committed: 	modified:   ntuaflixEpa/__pycache__/settings.cpython-312.pyc 	modified:   ntuaflixEpa/settings.py 	new file:   ntuaflixEpa_api/__pycache__/__init__.cpython-312.pyc 	new file:   ntuaflixEpa_api/__pycache__/admin.cpython-312.pyc 	new file:   ntuaflixEpa_api/__pycache__/apps.cpython-312.pyc 	new file:   ntuaflixEpa_api/__pycache__/models.cpython-312.pyc 	modified:   ntuaflixEpa_api/admin.py 	new file:   ntuaflixEpa_api/migrations/0001_initial.py 	new file:   ntuaflixEpa_api/migrations/__pycache__/0001_initial.cpython-312.pyc 	new file:   ntuaflixEpa_api/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   ntuaflixEpa_api/models.py
    Untracked files:
    	ntuaflixEpa_api/serializers.py

--- ntuaflixEpa_api/models.py
+++ ntuaflixEpa_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class TitleBasic(models.Model):
+    tconst = models.CharField(max_length=10, primary_key=True)
+    titleType = models.CharField(max_length=255, blank=True, null=True)
+    primaryTitle = models.CharField(max_length=255, blank=True, null=True)
+    originalTitle = models.CharField(max_length=255, blank=True, null=True)
+    isAdult = models.IntegerField(blank=True, null=True)
+    startYear = models.IntegerField(blank=True, null=True)
+    endYear = models.IntegerField(blank=True, null=True)
+    runtimeMinutes = models.IntegerField(blank=True, null=True)
+    genres = models.CharField(max_length=255, blank=True, null=True)
+    img_url_asset = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        db_table = 'titlebasic'  # Exact table name as in MySQL
+
+    def __str__(self):
+        return self.primaryTitle
